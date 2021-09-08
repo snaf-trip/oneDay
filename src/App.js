46eb4b24af219243a3fe07655b2522b58deffe5e
Егор <egornesvickij@gmail.com>
2021-09-08 23:05:54 +0500

    Deleted container and added normalize

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Container from './components/container/container';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <h1>TodoList ONEDAY</h1>
-      </Container>
     </div>
   );
 }
