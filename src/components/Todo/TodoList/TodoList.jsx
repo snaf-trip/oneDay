11abef14e2ffe9429e29abbdc41cfcfafc7dafc6
Егор <egornesvickij@gmail.com>
2021-09-09 11:15:47 +0500

    change file strukture

--- src/components/Todo/TodoList/TodoList.jsx
+++ src/components/Todo/TodoList/TodoList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import './TodoList.css';
-import TodoItem from "./TodoItem";
+import TodoItem from "../TodoItem/TodoItem.jsx";
 
 function TodoList(props) {
   return (
