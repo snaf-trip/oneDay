94328d41e9b84b4821bbbdb11f5a32cd04ab18c6
Егор <egornesvickij@gmail.com>
2021-09-20 14:19:26 +0500

    now the modal window with the task information opens correctly

--- src/components/Modal/ModalContent/infoTask/infoTask.jsx
+++ src/components/Modal/ModalContent/infoTask/infoTask.jsx
@@ -10,11 +10,12 @@ function InfoTask({ setActive, todo, modalContent }) {
       </div>
       {todo !== undefined ?
         <>
-          <input type="text" className='modal-input-title' placeholder='Введите название задачи' defaultValue={todo.title} />
-          <textarea className='modal-input-description' placeholder='Введите описание задачи' defaultValue={todo.description}></textarea>
-          <input type="time" name="deadline" id="deadline" min='17:00' max='21:00' defaultValue={todo.deadline} />
+          {/* key в inputs используется для запуска повторного рендеринга, чтобы значения в инпутах изменялись */}
+          <input type="text" className='modal-input-title' placeholder='Введите название задачи' defaultValue={todo.title} key={`${Math.floor((Math.random() * 1000))}-min`} />
+          <textarea className='modal-input-description' placeholder='Введите описание задачи' defaultValue={todo.description} key={`${Math.floor((Math.random() * 1000))}-min`}></textarea>
+          <input type="time" name="deadline" id="deadline" min='17:00' max='21:00' defaultValue={todo.deadline} key={`${Math.floor((Math.random() * 1000))}-min`} />
           <label htmlFor="deadline">До сколи нужно выполнить</label>
-          <input type="checkbox" name="important" id="important" defaultChecked={todo.important} />
+          <input type="checkbox" name="important" id="important" defaultChecked={todo.important} key={`${Math.floor((Math.random() * 1000))}-min`} />
           <label htmlFor="important">Важная задача</label>
         </>
         :
