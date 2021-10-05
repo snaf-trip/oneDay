4ebd880d3663d68224a96d7caf70447d0284874d
Егор <egornesvickij@gmail.com>
2021-10-05 10:50:17 +0500

    added new styles for todo item

--- src/components/Todo/TodoItem/TodoItem.jsx
+++ src/components/Todo/TodoItem/TodoItem.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes, { func } from 'prop-types';
 import './TodoItem.css';
+import importantIcon from '../../../img/important.svg';
+import deleteTaskIcon from '../../../img/deleteTask.svg';
 
 function TodoItem({ todo, completedChange, checked, deleteTask, setActive, setContent, setTodo }) {
 
@@ -12,9 +14,27 @@ function TodoItem({ todo, completedChange, checked, deleteTask, setActive, setCo
 
   return (
     <li {...todo.completed === true ? { className: 'todo-item' + ' ' + 'done' } : { className: 'todo-item' }} >
-      <input type="checkbox" onChange={() => completedChange(todo.id)} checked={checked} />
-      <span onClick={() => { openModalInfoTask(todo.id) }}>{todo.title}</span>
-      <button onClick={() => deleteTask(todo.id)}>x</button>
+      <input type="checkbox" className='checkbox-input' onChange={() => completedChange(todo.id)} checked={checked} />
+      <span className='task-title' onClick={() => { openModalInfoTask(todo.id) }}>{todo.title}</span>
+      <div className='additional-content'>
+        <div className='block-deadline'>
+          {todo.deadline !== '' ?
+            todo.deadline
+            :
+            null
+          }
+        </div>
+
+        <div className='block-important'>
+          {todo.important === true ?
+            <img src={importantIcon} width='21' height='21' />
+            :
+            null
+          }
+        </div>
+
+        <a className='delete-task-button' onClick={() => deleteTask(todo.id)}><img src={deleteTaskIcon} /></a>
+      </div>
     </li >
   )
 }
