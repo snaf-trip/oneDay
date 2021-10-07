7bb40a4f276ca2c67c1a455ff8faa63d9d3ec902
Егор <egornesvickij@gmail.com>
2021-10-07 10:35:47 +0500

    new styles for modal (infp task)

--- src/components/Modal/ModalContent/infoTask/infoTask.jsx
+++ src/components/Modal/ModalContent/infoTask/infoTask.jsx
@@ -18,23 +18,31 @@ function InfoTask({ setActive, todo, modalContent, todos, saveTodos }) {
     <>
       <div className="modal-header">
         <h2 className="modal-title">Информация</h2>
-        <button className="modal-close-button" onClick={() => { setActive(false) }}>x</button>
+        <a className="modal-close-button" onClick={() => { setActive(false) }}></a>
       </div>
       {todo !== undefined ?
         <>
           {/* key в inputs используется для запуска повторного рендеринга, чтобы значения в инпутах изменялись */}
           <input type="text" className='modal-input-title' placeholder='Введите название задачи' defaultValue={todo.title} key={keyForTitle} />
           <textarea className='modal-input-description' placeholder='Введите описание задачи' defaultValue={todo.description} key={keyForDescription}></textarea>
-          <input type="time" name="deadline" id="deadline" min='17:00' max='21:00' defaultValue={todo.deadline} key={keyForTime} />
+          <div className='deadline-content'>
+            <label htmlFor="deadline" className='deadline-label'>До сколи нужно выполнить</label>
+            <input type="time" name="deadline" id="deadline" className='deadline-input' min='17:00' max='21:00' defaultValue={todo.deadline} key={keyForTime} />
+          </div>
+          <label htmlFor="important" className='important-label'>Важная задача</label>
+          <input type="checkbox" name="important" id="important" className='important-input' defaultChecked={todo.important} key={keyForImportant} />
+          {/* <input type="time" name="deadline" id="deadline" min='17:00' max='21:00' defaultValue={todo.deadline} key={keyForTime} />
           <label htmlFor="deadline">До сколи нужно выполнить</label>
           <input type="checkbox" name="important" id="important" defaultChecked={todo.important} key={keyForImportant} />
-          <label htmlFor="important">Важная задача</label>
+          <label htmlFor="important">Важная задача</label> */}
         </>
         :
         null
       }
-      <button className='modal-create-button' onClick={() => { saveEditTask(todo, todos, saveTodos) }}>сохранить</button>
-      <button className='modal-cancel-button' onClick={() => { setActive(false) }}>отмена</button>
+      <div className='buttons'>
+        <a className='modal-cancel-button' onClick={() => { setActive(false) }}>отмена</a>
+        <a className='modal-create-button' onClick={() => saveEditTask(todo, todos, saveTodos)}>сохранить</a>
+      </div>
     </>
   )
 }
