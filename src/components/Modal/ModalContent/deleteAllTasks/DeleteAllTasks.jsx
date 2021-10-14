b356f5bea8073bc2189c99842b7fae538508b81f
Егор <egornesvickij@gmail.com>
2021-10-14 11:00:20 +0500

    test design for modal (delete all)

--- src/components/Modal/ModalContent/deleteAllTasks/DeleteAllTasks.jsx
+++ src/components/Modal/ModalContent/deleteAllTasks/DeleteAllTasks.jsx
@@ -5,16 +5,16 @@ import { deleteAllTasks } from "../../../../utils/deleteAllTasks.js";
 
 function DeleteAllTasks({ setActive, saveTodos }) {
   return (
-    <>
+    <div className='delete-all--content'>
       <div className="modal-header--delete">
         <h2 className="modal-title modal-title--delete">Очистить весь список задач</h2>
         <span className='description-modal--delete'>Вы уверены что хотите очистить все имеющиеся данные?</span>
       </div>
-      <div className='buttons'>
-        <a className='modal-cancel-button' onClick={() => { setActive(false) }}>Отмена</a>
-        <a className='modal-create-button' onClick={() => { deleteAllTasks(saveTodos, setActive) }}>Очистить</a>
+      <div className='buttons--delete'>
+        <a className='modal-cancel-button--delete' onClick={() => { setActive(false) }}>Отмена</a>
+        <a className='modal-create-button--delete' onClick={() => { deleteAllTasks(saveTodos, setActive) }}>Очистить</a>
       </div>
-    </>
+    </div>
   )
 }
 
