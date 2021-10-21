10d73206a3aa41ae0a40539ee6bf8782848c7f76
Егор <egornesvickij@gmail.com>
2021-10-22 00:07:57 +0500

    cannot save without changes

--- src/utils/saveEditTask.js
+++ src/utils/saveEditTask.js
@@ -4,6 +4,13 @@ function saveEditTask(todo, todos, saveTodos, setActive) {
   let taskDeadline = document.getElementById('deadline').value;
   let taskImportant = document.getElementById('important').checked;
 
+  let initialVersion = {
+    title: todo.title,
+    description: todo.description,
+    deadline: todo.deadline,
+    important: todo.important,
+  };
+
   let taskChanged = todos.map(item => {
     if (item === todo) {
       item.title = taskTitle;
@@ -13,12 +20,20 @@ function saveEditTask(todo, todos, saveTodos, setActive) {
     }
     return item
   })
-  localStorage.removeItem('todos');
-  localStorage.setItem('todos', JSON.stringify(taskChanged));
-  saveTodos(taskChanged);
 
-  alert('Изменения успешно сохранены');
-  console.log(todo);
+  if (
+    todo.title !== initialVersion.title ||
+    todo.description !== initialVersion.description ||
+    todo.deadline !== initialVersion.deadline ||
+    todo.important !== initialVersion.important
+  ) {
+    localStorage.removeItem('todos');
+    localStorage.setItem('todos', JSON.stringify(taskChanged));
+    saveTodos(taskChanged);
+    alert('Изменения успешно сохранены!');
+  } else {
+    alert('Вы не внесли никаких изменений.')
+  }
 };
 
 export { saveEditTask };
