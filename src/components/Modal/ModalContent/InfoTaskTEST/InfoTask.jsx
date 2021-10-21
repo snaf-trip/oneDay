cf80a2ae191f63118d02c7747888236c9581067d
Егор <egornesvickij@gmail.com>
2021-10-21 22:03:26 +0500

    update modal (info task),remove cross for close

--- src/components/Modal/ModalContent/InfoTaskTEST/InfoTask.jsx
+++ src/components/Modal/ModalContent/InfoTaskTEST/InfoTask.jsx
@@ -18,7 +18,6 @@ function InfoTask({ setActive, todo, modalContent, todos, saveTodos }) {
     <>
       <div className="modal-header">
         <h2 className="modal-title">Информация</h2>
-        <a className="modal-close-button" onClick={() => { setActive(false) }}></a>
       </div>
       {todo !== undefined ?
         <>
@@ -29,8 +28,10 @@ function InfoTask({ setActive, todo, modalContent, todos, saveTodos }) {
             <label htmlFor="deadline" className='deadline-label'>До сколи нужно выполнить</label>
             <input type="time" name="deadline" id="deadline" className='deadline-input' min='17:00' max='21:00' defaultValue={todo.deadline} key={keyForTime} />
           </div>
-          <label htmlFor="important" className='important-label'>Важная задача</label>
-          <input type="checkbox" name="important" id="important" className='important-input' defaultChecked={todo.important} key={keyForImportant} />
+          <div className="important-content">
+            <label htmlFor="important" className='important-label'>Важная задача</label>
+            <input type="checkbox" name="important" id="important" className='important-input' defaultChecked={todo.important} key={keyForImportant} />
+          </div>
           {/* <input type="time" name="deadline" id="deadline" min='17:00' max='21:00' defaultValue={todo.deadline} key={keyForTime} />
           <label htmlFor="deadline">До сколи нужно выполнить</label>
           <input type="checkbox" name="important" id="important" defaultChecked={todo.important} key={keyForImportant} />
