b58b038e329763612e0a7ee81afb2ebd07c010e3
Егор <egornesvickij@gmail.com>
2021-11-16 01:09:19 +0500

    min fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,14 @@ function Header({ addTask, delTasks }) {
     <div className='header'>
       <div className='vers-title'>
         <h1 className='app-title'>OneDay</h1>
-        <span className='version'>v 0.1</span>
+        <span className='version'>v 1.0.0</span>
       </div>
       <div className='header-buttons'>
         <a onClick={() => addTask()} className='header-button header-button--add'>
         </a>
         <a onClick={() => delTasks()} className='header-button header-button--del'>
         </a>
-        {/* Это кнопа для темной темы!!!!!!
+        {/* Button for dark theme...
         <a className='header-button header-button--theme'></a> */}
       </div>
     </div>
